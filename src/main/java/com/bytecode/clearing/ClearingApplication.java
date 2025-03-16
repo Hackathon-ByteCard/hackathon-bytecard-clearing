301fe7bfd6546ed31b450a9c82a2d9a971a33df5
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-03-16 19:34:50 -0300

    first commit

---
+++ src/main/java/com/bytecode/clearing/ClearingApplication.java
@@ -0,0 +1,13 @@
+package com.bytecode.clearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytecode/clearing/ClearingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecode.clearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
